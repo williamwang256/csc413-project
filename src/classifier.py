0ade7b80427385b5517da13d05d9f5c8e69f8003
William Wang <williamm.wang@mail.utoronto.ca>
2024-04-14 15:09:39 -0400

    scripts

--- src/classifier.py
+++ src/classifier.py
@@ -8,60 +8,53 @@ from BirdSpectrogramDataset import *
 from BirdSpectrogramResNet50 import *
 from config import *
 
-# move data to GPU
-def get_default_device():
-  if torch.cuda.is_available():
-    return torch.device('cuda:0')
-  else:
-    return torch.device('cpu')
 
-if __name__ == "__main__":
-  device = get_default_device()
-  print("Device: ", device)
+device = torch.device('cuda:0') if torch.cuda.is_available() else torch.device('cpu')
+print("Device: ", device)
 
-  random_seed = 45
-  torch.manual_seed(random_seed)
+random_seed = 45
+torch.manual_seed(random_seed)
 
-  # use ImageFolder to load the data to Pytorch Dataset Format
-  dataset = ImageFolder(SORTED_PATH) # REPLACE WITH CORRECT PATH
-  print("Number of samples: ", len(dataset))
-  # print(dataset.classes)
-  print("Number of classes: ", len(dataset.classes))
+# use ImageFolder to load the data to Pytorch Dataset Format
+dataset = ImageFolder(SORTED_PATH) # REPLACE WITH CORRECT PATH
+print("Number of samples: ", len(dataset))
+# print(dataset.classes)
+print("Number of classes: ", len(dataset.classes))
 
-  # UPDATE THESE RATIOS ONCE WE HAVE THE FULL DATASET
-  train_size = int(0.6 * len(dataset))
-  val_size   = int(0.2 * len(dataset))
-  test_size  = len(dataset) - train_size - val_size
+# UPDATE THESE RATIOS ONCE WE HAVE THE FULL DATASET
+train_size = int(0.6 * len(dataset))
+val_size   = int(0.2 * len(dataset))
+test_size  = len(dataset) - train_size - val_size
 
-  train_set, val_set, test_set = random_split(dataset, [train_size, val_size, test_size])
-  print("Train set size: ", len(train_set))
-  print("Validation set size: ", len(val_set))
-  print("Test set size: ", len(test_set))
+train_set, val_set, test_set = random_split(dataset, [train_size, val_size, test_size])
+print("Train set size: ", len(train_set))
+print("Validation set size: ", len(val_set))
+print("Test set size: ", len(test_set))
 
-  # define transformations here
-  transform = transforms.Compose([
-    # you can add other transformations in this list
-    transforms.Resize((224, 224)),
-    transforms.ToTensor()
-  ])
+# define transformations here
+transform = transforms.Compose([
+  # you can add other transformations in this list
+  transforms.Resize((224, 224)),
+  transforms.ToTensor()
+])
 
-  transform_aug = transforms.Compose([
-    # you can add other transformations in this list
-    transforms.Resize((224, 224)),
-    transforms.ToTensor(),
-    transforms.Lambda(lambda x: torch.roll(x, randint(1, 10000), dims=2)),
-  ])
+transform_aug = transforms.Compose([
+  # you can add other transformations in this list
+  transforms.Resize((224, 224)),
+  transforms.ToTensor(),
+  transforms.Lambda(lambda x: torch.roll(x, randint(1, 10000), dims=2)),
+])
 
-  # define dataset
-  train_ds = BirdSpectrogramDataset(train_set, transform_aug)
-  val_ds   = BirdSpectrogramDataset(val_set, transform)
-  test_ds  = BirdSpectrogramDataset(test_set, transform)
+# define dataset
+train_ds = BirdSpectrogramDataset(train_set, transform_aug)
+val_ds   = BirdSpectrogramDataset(val_set, transform)
+test_ds  = BirdSpectrogramDataset(test_set, transform)
 
-  model = BirdSpectrogramResNet50(len(dataset.classes))
+model = BirdSpectrogramResNet50(len(dataset.classes))
 
-  # Train the model
-  train_model(model, device, train_ds, val_ds, learning_rate=0.001, epochs=10, plot_every=1, plot=True)
+# Train the model
+train_model(model, device, train_ds, val_ds, learning_rate=0.001, epochs=10, plot_every=1, plot=True)
 
-  # Final test accuracy
-  test_dl  = torch.utils.data.DataLoader(test_ds, 256)
-  print("test acc {}".format(accuracy(model, test_dl, device)))
+# Final test accuracy
+test_dl  = torch.utils.data.DataLoader(test_ds, 256)
+print("test acc {}".format(accuracy(model, test_dl, device)))
--- src/transformer.py
+++ src/transformer.py
@@ -4,81 +4,102 @@
 
 import torch
 import numpy as np
-from datasets import load_metric, load_dataset
+from random import randint
+from datasets import load_metric, load_dataset, DatasetDict
 from transformers import Trainer, TrainingArguments, ViTForImageClassification, ViTImageProcessor
 
 from config import *
 
 
+# We will fine-tune the following pre-trained model
+model_name_or_path = "google/vit-base-patch16-224-in21k"
+
+# Load the dataset, and split into train, test, and validation sets
+# Use the split: 60% train, 20% validation, 20% test
+full = load_dataset("imagefolder", data_dir=SORTED_PATH, split="train")
+train_testvalid = full.train_test_split(test_size=0.4)
+test_valid = train_testvalid['test'].train_test_split(test_size=0.5)
+ds = DatasetDict({
+    'train': train_testvalid['train'],
+    'test': test_valid['test'],
+    'valid': test_valid['train']})
+
+# Load the pre-trained model's preprocessor
+processor = ViTImageProcessor.from_pretrained(model_name_or_path)
+
+# A transformation which applies the above processor and some data
+# augmentation steps (time shifting)
 def transform(example_batch):
   inputs = processor([x for x in example_batch["image"]], return_tensors="pt")
+  inputs["pixel_values"] = torch.roll(inputs["pixel_values"], randint(1, 10000), dims=3)
   inputs["label"] = example_batch["label"]
   return inputs
 
+# Apply the transformation to the dataset. Note: the transformation is applied
+# to examples only as they are indexed
+prepared_ds = ds.with_transform(transform)
 
+# Create our model
+labels = ds["train"].features["label"].names
+model = ViTForImageClassification.from_pretrained(
+  model_name_or_path,
+  num_labels=len(labels),
+  id2label={str(i): c for i, c in enumerate(labels)},
+  label2id={c: str(i) for i, c in enumerate(labels)}
+)
+
+# Customizable training arguments
+training_args = TrainingArguments(
+  output_dir="./vit-base-birds",
+  per_device_train_batch_size=16,
+  evaluation_strategy="steps",
+  num_train_epochs=10,
+  fp16=True,
+  save_steps=10,
+  eval_steps=10,
+  logging_steps=10,
+  learning_rate=2e-4,
+  save_total_limit=2,
+  remove_unused_columns=False,
+  push_to_hub=False,
+  report_to="tensorboard",
+  load_best_model_at_end=True,
+)
+
+# Stacks the inputs from a batch into a single tensor
 def collate_fn(batch):
   return {
-    "pixel_values": torch.stack([x["pixel_values"] for x in batch]),
-    "labels": torch.tensor([x["label"] for x in batch])
+    "pixel_values" : torch.stack([x["pixel_values"] for x in batch]),
+    "labels" : torch.tensor([x["label"] for x in batch])
   }
 
-
+# Computes accuracy metric
+metric = load_metric("accuracy")
 def compute_metrics(p):
-  return metric.compute(predictions=np.argmax(p.predictions, axis=1), references=p.label_ids)
+  return metric.compute(predictions=np.argmax(p.predictions, axis=1),
+                        references=p.label_ids)
 
+# Create the trainer object
+trainer = Trainer(
+  model=model,
+  args=training_args,
+  data_collator=collate_fn,
+  compute_metrics=compute_metrics,
+  train_dataset=prepared_ds["train"],
+  eval_dataset=prepared_ds["valid"],
+  tokenizer=processor,
+)
 
-if __name__ == "__main__":
+# Train the model 
+train_results = trainer.train()
 
-  ds = load_dataset("imagefolder", data_dir=SORTED_PATH, split="train")
-  ds = ds.train_test_split(test_size=0.2)
+# Save the results
+trainer.save_model()
+trainer.log_metrics("train", train_results.metrics)
+trainer.save_metrics("train", train_results.metrics)
+trainer.save_state()
 
-  model_name_or_path = "google/vit-base-patch16-224-in21k"
-  processor = ViTImageProcessor.from_pretrained(model_name_or_path)
-
-  prepared_ds = ds.with_transform(transform)
-  metric = load_metric("accuracy")
-  labels = ds["train"].features["label"].names
-
-  model = ViTForImageClassification.from_pretrained(
-    model_name_or_path,
-    num_labels=len(labels),
-    id2label={str(i): c for i, c in enumerate(labels)},
-    label2id={c: str(i) for i, c in enumerate(labels)}
-  )
-
-  training_args = TrainingArguments(
-    output_dir="./vit-base-birds",
-    per_device_train_batch_size=16,
-    evaluation_strategy="steps",
-    num_train_epochs=10,
-    fp16=True,
-    save_steps=100,
-    eval_steps=100,
-    logging_steps=10,
-    learning_rate=2e-4,
-    save_total_limit=2,
-    remove_unused_columns=False,
-    push_to_hub=False,
-    report_to="tensorboard",
-    load_best_model_at_end=True,
-  )
-
-  trainer = Trainer(
-    model=model,
-    args=training_args,
-    data_collator=collate_fn,
-    compute_metrics=compute_metrics,
-    train_dataset=prepared_ds["train"],
-    eval_dataset=prepared_ds["test"],
-    tokenizer=processor,
-  )
-
-  train_results = trainer.train()
-  trainer.save_model()
-  trainer.log_metrics("train", train_results.metrics)
-  trainer.save_metrics("train", train_results.metrics)
-  trainer.save_state()
-
-  metrics = trainer.evaluate(prepared_ds["test"])
-  trainer.log_metrics("eval", metrics)
-  trainer.save_metrics("eval", metrics)
+# Finally, evaluate on the test set
+metrics = trainer.evaluate(prepared_ds["test"])
+trainer.log_metrics("eval", metrics)
+trainer.save_metrics("eval", metrics)
