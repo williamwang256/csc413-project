4518792fee06e8b3aac583b65065caecba02fc52
William Wang <williamm.wang@mail.utoronto.ca>
2024-04-15 00:23:42 -0400

    transformer and rename preprocess

--- src/transformer.py
+++ src/transformer.py
@@ -5,14 +5,17 @@
 import torch
 import numpy as np
 from random import randint
-from datasets import load_metric, load_dataset, DatasetDict
-from transformers import Trainer, TrainingArguments, ViTForImageClassification, ViTImageProcessor
+from evaluate import load
+from datasets import load_dataset, DatasetDict
+from transformers import Trainer, TrainingArguments, ViTForImageClassification, ViTImageProcessor, ViTFeatureExtractor
+from PIL import Image
+import cv2
+import matplotlib.pyplot as plt
 
 from config import *
 
-
 # We will fine-tune the following pre-trained model
-model_name_or_path = "google/vit-base-patch16-224-in21k"
+MODEL = "google/vit-base-patch16-224-in21k"
 
 # Load the dataset, and split into train, test, and validation sets
 # Use the split: 60% train, 20% validation, 20% test
@@ -25,7 +28,7 @@ ds = DatasetDict({
     'valid': test_valid['train']})
 
 # Load the pre-trained model's preprocessor
-processor = ViTImageProcessor.from_pretrained(model_name_or_path)
+processor = ViTImageProcessor.from_pretrained(MODEL)
 
 # A transformation which applies the above processor and some data
 # augmentation steps (time shifting)
@@ -42,7 +45,7 @@ prepared_ds = ds.with_transform(transform)
 # Create our model
 labels = ds["train"].features["label"].names
 model = ViTForImageClassification.from_pretrained(
-  model_name_or_path,
+  MODEL,
   num_labels=len(labels),
   id2label={str(i): c for i, c in enumerate(labels)},
   label2id={c: str(i) for i, c in enumerate(labels)}
@@ -74,7 +77,7 @@ def collate_fn(batch):
   }
 
 # Computes accuracy metric
-metric = load_metric("accuracy")
+metric = load("accuracy")
 def compute_metrics(p):
   return metric.compute(predictions=np.argmax(p.predictions, axis=1),
                         references=p.label_ids)
@@ -103,3 +106,53 @@ trainer.save_state()
 metrics = trainer.evaluate(prepared_ds["test"])
 trainer.log_metrics("eval", metrics)
 trainer.save_metrics("eval", metrics)
+
+def get_attention_map(img, get_mask=False):
+  # Pass the input through the model
+  inputs  = processor(images=img, return_tensors="pt")
+  print(torch.argmax(model(**inputs).logits, dim=1))
+  output = model(**inputs, output_attentions=True)
+
+  # Access attention maps
+  att_mat = torch.stack(output.attentions).squeeze(1)
+
+  # Average the attention weights across all heads.
+  att_mat = torch.mean(att_mat, dim=1)
+
+  # To account for residual connections, we add an identity matrix to the
+  # attention matrix and re-normalize the weights.
+  residual_att = torch.eye(att_mat.size(1))
+  aug_att_mat = att_mat + residual_att
+  aug_att_mat = aug_att_mat / aug_att_mat.sum(dim=-1).unsqueeze(-1)
+
+  # Recursively multiply the weight matrices
+  joint_attentions = torch.zeros(aug_att_mat.size())
+  joint_attentions[0] = aug_att_mat[0]
+
+  for n in range(1, aug_att_mat.size(0)):
+    joint_attentions[n] = torch.matmul(aug_att_mat[n], joint_attentions[n-1])
+
+  v = joint_attentions[-1]
+  grid_size = int(np.sqrt(aug_att_mat.size(-1)))
+  mask = v[0, 1:].reshape(grid_size, grid_size).detach().numpy()
+  if get_mask:
+    result = cv2.resize(mask / mask.max(), img.size)
+  else:        
+    mask = cv2.resize(mask / mask.max(), img.size)[..., np.newaxis]
+    result = (mask * img).astype("uint8")
+  
+  return result
+
+def plot_attention_map(original_img, att_map):
+  fig, (ax1, ax2) = plt.subplots(ncols=2, figsize=(16, 16))
+  ax1.set_title('Original')
+  ax2.set_title('Attention Map Last Layer')
+  _ = ax1.imshow(original_img)
+  _ = ax2.imshow(att_map)
+  plt.axis('off')
+  plt.savefig("map.jpg")
+
+# Plot attention map
+img = Image.open(SPECTROGRAM_PATH + "/Barn Swallow/xc157331_039.jpg")
+result = get_attention_map(img, True)
+plot_attention_map(img, result)
