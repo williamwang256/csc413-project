c9e3c021d66bb781ad451c74a9e3b9b8d6823c1c
William Wang <williamm.wang@mail.utoronto.ca>
2024-04-18 10:37:34 -0400

    config file

--- src/config.py
+++ src/config.py
@@ -1,10 +1,6 @@
 BASE = "/h/u6/c9/01/wangwi18/winter24/csc413/project/"
-
 SONGS_PATH = BASE + "/data/songs/songs/"
 METADATA_PATH = BASE + "/data/birdsong_metadata.csv"
 AUDIO_PATH = BASE + "/data/audio/"
 SPECTROGRAM_PATH = BASE + "/data/spectrograms/"
-
-MODEL_SAVE_DIR = BASE + "vit-birds"
-
 PLOTS_DIR = BASE + "/plots/"
